76175150d6fa1820153de685f537377c9fcc26a6
frodo821 <sakaic2003@gmail.com>
2019-08-01 18:52:12 +0900

    add: initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <header></header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
